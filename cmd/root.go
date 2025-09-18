80dba2b9c4e16b33494d02c2c9058ba5abfcd85a
Raxuis <raclot.raphael@gmail.com>
2025-09-18 20:53:18 +0200

    feat(main): refactor CLI structure and move command logic to separate package

---
+++ cmd/root.go
@@ -0,0 +1,24 @@
+package cmd
+
+import (
+	"fmt"
+	"github.com/spf13/cobra"
+)
+
+var rootCmd = &cobra.Command{
+	Use:   "raxuiscli",
+	Short: "A powerful CLI toolkit",
+	Long:  `RaxuisCLI is a collection of the most used commands by @Raxuis.`,
+	Run: func(cmd *cobra.Command, args []string) {
+		fmt.Println("Welcome to RaxuisCLI! Use --help to see available commands.")
+	},
+}
+
+func Execute() error {
+	return rootCmd.Execute()
+}
+
+func init() {
+	//👇 Global flags can be added here
+	rootCmd.PersistentFlags().BoolP("verbose", "v", false, "verbose output")
+}
--- main.go
+++ main.go
@@ -1,32 +1,13 @@
 package main
 
 import (
-	"fmt"
-	"os"
+	"log"
 
-	"github.com/spf13/cobra"
+	"RaxuisCLI/cmd"
 )
 
 func main() {
-	var name string
-
-	var rootCmd = &cobra.Command{
-		Use:   "raxuiscli",
-		Short: "A simple greeting CLI",
-		Long:  "This CLI prints a greeting message to the user",
-		Run: func(cmd *cobra.Command, args []string) {
-			if name == "" {
-				fmt.Println("Hello, World!")
-			} else {
-				fmt.Printf("Hello, %s!\n", name)
-			}
-		},
-	}
-
-	rootCmd.Flags().StringVarP(&name, "name", "n", "", "Your name")
-
-	if err := rootCmd.Execute(); err != nil {
-		fmt.Println(err)
-		os.Exit(1)
+	if err := cmd.Execute(); err != nil {
+		log.Fatal(err)
 	}
 }
